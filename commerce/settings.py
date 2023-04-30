ffe197cb55e30bddb1665c60dc480babbac01a85
shen20 <sanantoniosheleen@gmail.com>
2023-04-30 15:18:20 -0700

    Added medial url in settings.py

--- commerce/settings.py
+++ commerce/settings.py
@@ -130,4 +130,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'assets', 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'assets', 'media', 'media/media')
