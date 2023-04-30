a76ae75ea4bd72f123d81875e8a48761f76a7408
shen20 <sanantoniosheleen@gmail.com>
2023-04-30 02:48:10 -0700

    Added requirements.txt

--- commerce/wsgi.py
+++ commerce/wsgi.py
@@ -8,9 +8,20 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
+
+# assuming your Django settings file is at '/home/myusername/mysite/mysite/settings.py'
+path = '/home/myusername/commerce-app'
+if path not in sys.path:
+    sys.path.insert(0, path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'commerce.settings'
+
+## Uncomment the lines below depending on your Django version
+###### then, for Django >=1.5:
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'commerce.settings')
-
 application = get_wsgi_application()
+###### or, for older Django <=1.4
+#import django.core.handlers.wsgi
+#application = django.core.handlers.wsgi.WSGIHandler()
