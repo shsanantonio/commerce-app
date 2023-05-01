1fb92099fa710c5c4ddd97a39dfbb84e1ba2afa9
shen20 <sanantoniosheleen@gmail.com>
2023-04-30 17:43:51 -0700

    test

--- commerce/settings.py
+++ commerce/settings.py
@@ -124,11 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'home/shen001/commerce-app/staticfiles'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 MEDIA_URL = '/media/'
-
-MEDIA_ROOT = 'home/shen001/commerce-app/media/media'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'assets', 'media', 'media/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media').replace('\\', '/')
--- commerce/urls.py
+++ commerce/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
-from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("auctions.urls"))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
